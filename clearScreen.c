032ef7a3fe49b9b033fdc02010aefea182895cf6
Marcelo <maisnada@gmail.com>
2025-09-17 23:08:13 -0300

    clearScreen test

--- clearScreen.c
+++ clearScreen.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(void){
 
+  int i = 0;
+
+  char bar[50] = "["; 
+
+  //bar[49] = "]";
+
+  while(i <= 47){
+
+    printf("%s\n", 2,strcat(bar, ">"));
+
+    
+
+    i++;
+
+    sleep(0.8);
+  }
+
+  /*
   system("cls || clear");
 
   printf("Clear screen complete!\n\n");
-
+  */
   return 0;
 }
